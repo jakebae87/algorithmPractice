2646b234d90d19897f9d024f44cb552464f78855
jakebae87 <jeonghyun.bae@kakao.com>
2023-09-04 17:37:08 +0900

    [코드업1082] 풀이 완료

--- src/codeup/CodeUp1082.java
+++ src/codeup/CodeUp1082.java
@@ -1,17 +1,24 @@
 package codeup;
+
 import java.util.Scanner;
 
 public class CodeUp1082 {
 
 	public static void main(String[] args) {
 //		https://www.codeup.kr/problem.php?id=1082	16진수 구구단?
-//		풀이중
+//		2023-09-04
 
 		Scanner scan = new Scanner(System.in);
-		int input = scan.nextInt(16);
+		String input = scan.nextLine();
 
-		for (int i = 1; i <= 16; i++) {
-			System.out.printf("%x", i * input);
+		int hexa = Integer.valueOf(input, 16);
+
+		String number, result;
+
+		for (int i = 1; i < 16; i++) {
+			number = Integer.toHexString(i).toUpperCase();
+			result = Integer.toHexString(i * hexa).toUpperCase();
+			System.out.println(input + "*" + Integer.toHexString(i).toUpperCase() + "=" + result);
 		}
 
 	}
